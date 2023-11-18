c0eebf942855a74a465dc405dae5d63a586ceca1
Lin Lee <leelin2602@gmail.com>
2023-11-18 07:16:16 +0000

    add docker compose file for mysqld and named

---
+++ config/init.sql
@@ -0,0 +1,55 @@
+E `nycu_me_dns`;
+
+CREATE USER IF NOT EXISTS 'nycu_dns'@'%' IDENTIFIED BY 'abc123';
+GRANT ALL PRIVILEGES ON `nycu_me_dns`.* TO 'nycu_dns'@'%';
+FLUSH PRIVILEGES;
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+      `Id` varchar(16) NOT NULL,
+      `name` varchar(256) NOT NULL,
+      `username` varchar(256) NOT NULL,
+      `password` varchar(100) NOT NULL DEFAULT '',
+      `status` varchar(16) NOT NULL,
+      `email` varchar(256) NOT NULL,
+      `limit` int(11) NOT NULL DEFAULT '2',
+      `isAdmin` int(11) NOT NULL DEFAULT '0',
+      PRIMARY KEY (`Id`),
+      UNIQUE KEY `Id_UNIQUE` (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `domains`
+--
+
+CREATE TABLE `domains` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `userId` varchar(16) NOT NULL,
+      `domain` text,
+      `regDate` datetime DEFAULT NULL,
+      `expDate` datetime DEFAULT NULL,
+      `status` tinyint(1) DEFAULT '1',
+      PRIMARY KEY (`id`),
+      KEY `userId_idx` (`userId`),
+      CONSTRAINT `domains_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4;
+
+--
+-- Table structure for table `records`
+--
+
+CREATE TABLE `records` (
+      `domain` int(11) NOT NULL,
+      `type` char(16) NOT NULL,
+      `value` varchar(256) DEFAULT NULL,
+      `ttl` int(11) NOT NULL,
+      `regDate` datetime DEFAULT NULL,
+      `expDate` datetime DEFAULT NULL,
+      `status` tinyint(1) DEFAULT '1',
+      KEY `domain_idx` (`domain`),
+      CONSTRAINT `records_ibfk_1` FOREIGN KEY (`domain`) REFERENCES `domains` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
