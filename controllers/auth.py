181f30c6836c183ae4773fb62640d20ede8a0c79
Lin Lee <leelin2602@gmail.com>
2023-11-20 05:09:05 +0000

    add authenticate function

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,11 +1,29 @@
 from flask import Response, request
 from main import app, g, nycu_oauth, authService
 
+
+@app.before_request
+def before_request():
+    
+    g.user = authService.authenticate_token(request.headers.get('Authorization'))
+
 @app.route("/oauth/<string:code>", methods = ['GET'])
 def get_token(code):
-
+    
     token = nycu_oauth.get_token(code)
+
     if token:
         return {"token": authService.issue_token(nycu_oauth.get_profile(token))}
     else:
         return {"message": "Invalid code."}, 401
+
+@app.route("/whoami/", methods = ['GET'])
+def whoami():
+
+    if g.user:
+        data = {}
+        data['uid'] = g.user['uid']
+        data['email'] = g.user['email']
+        return data
+    
+    return {"message": "Unauth."}, 401
--- services/authService.py
+++ services/authService.py
@@ -3,6 +3,16 @@ from datetime import timezone, datetime
 from models import db
 import jwt
 
+class UnauthorizedError(Exception):
+    def __init__(self, msg):
+        self.msg = str(msg)
+
+    def __str__(self):
+        return self.msg
+
+    def __repr__(self):
+        return "Unauthorized: " + self.msg
+
 class AuthService:
     def __init__(self, logger, jwt_secret, sql_engine):
         self.logger = logger
@@ -32,3 +42,24 @@ class AuthService:
             session.commit()
         token = jwt.encode(token, self.jwt_secret, algorithm="HS256")
         return token
+    
+    def authenticate_token(self, payload):
+        try:
+            if not payload:
+                raise UnauthorizedError("not logged")
+            payload = payload.split(' ')
+            if len(payload) != 2:
+                raise UnauthorizedError("invalid payload")
+            tokenType, token = payload
+            if tokenType != 'Bearer': 
+                raise UnauthorizedError("invalid payload")
+            try:
+                payload = jwt.decode(token, self.jwt_secret, algorithms=["HS256"])
+            except Exception as e:
+                self.logger.debug(e.__str__())
+                return None
+            return payload
+        except UnauthorizedError as e:
+            self.logger.debug(e.__str__())
+            return None
+
